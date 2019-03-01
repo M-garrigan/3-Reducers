f67de1d33103aab4eea10c08c3b9bb3be4510ed2
M-garrigan <mgarrigan99@gmail.com>
2019-03-01 10:16:42 -0500

    Test change in port address

--- server/index.js
+++ server/index.js
@@ -16,6 +16,6 @@ app.get('/', (req, res) => {
   res.send();
 });
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`listening on port ${port}...`));
