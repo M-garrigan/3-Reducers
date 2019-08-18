bb3d7367b71f5c5de2054fd295e52c8eae0acc96
M-garrigan <mgarrigan99@gmail.com>
2019-08-18 04:56:45 -0400

    Changed file extention to .js from .jsx

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './components/App.jsx';
+import App from './components/App.js';
 
 
 ReactDOM.render(<App />, document.getElementById('app'));
