9297cc77db811ba96a70901906e37ef16b7d0a00
M-garrigan <mgarrigan99@gmail.com>
2019-08-20 15:20:06 -0400

    Laying the ground work for a time scrolling map of the world

--- src/components/Geo.js
+++ src/components/Geo.js
@@ -1,13 +1,40 @@
 
-import React from 'react';
+import React, { useState, useCallback, useLayoutEffect } from 'react';
 
-import '../styles/Main.css';
+import { geoEquirectangular, geoPath } from 'd3-geo';
+import { select } from 'd3-selection';
+
+import geoData from '../assets/GeoWorld.js';
+
+import '../styles/Geo.css';
+
 
 export default props => {
+
+  const width = Math.floor(props.dimensions.width * 0.95);
+
+  const projection = geoEquirectangular()
+    .fitExtent([[0, 0], [width, width / 2]], geoData)
+      // .scale(width / 2 / Math.PI)
+      // .scale(100)
+      // .translate([width / 2, width / 4])
+
+  const path = geoPath()
+    .projection(projection);
+
     
   return (
     <div className="geo-wrapper">
-      
+      <svg
+        className="geo-svg"
+        height={width / 2}
+        width={width}
+      >
+        <path
+          ref={node => select(node).datum(geoData).attr('d', path)}
+          // d={}
+        ></path>
+      </svg>
     </div>
   )
 }
