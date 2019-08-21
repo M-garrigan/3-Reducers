eef549187c7b73eb3446c25f08267c9bdf06fe08
M-garrigan <mgarrigan99@gmail.com>
2019-08-21 19:35:21 -0400

    Removing the data for the selected streamer or game to its own 'card' component

--- src/components/RealTime.js
+++ src/components/RealTime.js
@@ -7,27 +7,23 @@ import '../styles/RealTime.css';
 export default props => {
 
   const width = Math.round(props.dimensions.width * 0.95);
-  const bannerHeight = Math.round(width * 0.15);
+  const bannerHeight = Math.round(width * 0.1);
+  const timeBannerHeight = Math.round(width * 0.05);
 
   const [top, setTop] = useState(5);
+  const [timeScale, setTimeScale] = useState(10);
     
   return (
     <div className="rt-wrapper">
       
       <div 
-        className="rt-info-banner-wrapper"
+        className="rt-banner-wrapper"
         style={{height: bannerHeight}}
       >
 
-        <div className="rt-featured-wrapper">
-          <div className="rt-featured-image"></div>
-          <div className="rt-featured-name"></div>
-          <div className="rt-featured-uptime"></div>
-          <div className="rt-featured-game"></div>
-          <div className="rt-featured-viewers"></div>
-        </div>
+        <h2>{`Top ${props.category} on Twitch`}</h2>
 
-        <div className="rt-button-ctrl-wrapper">
+        <div className="rt-button-wrapper">
           <p>Top</p>
           <button 
             className={top === 3
@@ -57,6 +53,45 @@ export default props => {
         dimensions={props.dimensions}
       />
 
+      <div 
+        className="rtchart-time-banner-wrapper"
+        style={{height: timeBannerHeight}}
+      >
+          <p>Adjust Time Scale</p>
+          <button 
+            className={timeScale === 5
+              ? "rt-time-button-active" 
+              : "rt-time-button"
+            }
+            onClick={ () => setTimeScale(5) }
+          >5 Min</button>
+          <button 
+            className={timeScale === 10
+              ? "rt-time-button-active" 
+              : "rt-time-button"
+            }
+            onClick={ () => setTimeScale(10) }
+          >10 Min</button>
+          <button 
+            className={timeScale === 30
+              ? "rt-time-button-active" 
+              : "rt-time-button"
+            }
+            onClick={ () => setTimeScale(30) }
+          >30 Min</button>
+        </div>
+
+
     </div>
   )
 }
+
+{/* <Card />
+
+<div className="rt-featured-wrapper">
+  <div className="rt-featured-image"></div>
+  <div className="rt-featured-name"></div>
+  <div className="rt-featured-uptime"></div>
+  <div className="rt-featured-game"></div>
+  <div className="rt-featured-viewers"></div>
+</div> */}
